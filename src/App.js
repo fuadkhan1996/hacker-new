e78d0154cc7f253dfa772d2e0195d4b250fc6411
fuadkhan1996 <fuad.khan@thedevden.co>
2020-02-28 15:24:33 +0500

    Removed all unwanted code from App.js

--- src/App.js
+++ src/App.js
@@ -1,79 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import spinner from './spinner.svg';
-import Newsfeeds from './components/Newsfeeds';
-import Pagination from './components/Pagination';
+import News from './components/ContainerComponents/News';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-       data: [],
-       currentPage: 1,
-       newsPerPage: 5,
-       isLoading: true
-    };
-  }
-
-  componentDidMount() {
-    this.getNews();
-  }
-
-  getNews() {
-    fetch("https://api.hnpwa.com/v0/news/1.json")
-    .then((response) => response.json())
-      .then((data) => {
-        this.setState({ data, isLoading: false });
-      })
-    .catch((error) => {
-      console.error("Error: ", error);
-    });
-  }
-
-  paginate = (currentPage) => {
-    this.setState({ currentPage })
-  }
-
-  render() {
-    const {data, currentPage, newsPerPage, isLoading} = this.state
-    const indexOfLastNews = currentPage * newsPerPage;
-    const indexOfFirstNews = indexOfLastNews - newsPerPage;
-    const currentNews = data.slice(indexOfFirstNews, indexOfLastNews)
-
-    return (
-      <div>
-        <h1 className="text-center" text="dark">News Feeds</h1>
-        {isLoading ? (
-            <div className="text-center">
-              <img src={ spinner } alt="logo" className="App-Spinner" />
-            </div>
-            )
-            :(
-              <div>
-              <Pagination
-              newsPerPage = { newsPerPage }
-              totalNews = { data.length }
-              currentPage = { currentPage }
-              paginate = { this.paginate }>
-            </Pagination>
-
-            <div className="bg-dark">
-              <Newsfeeds data = { currentNews }/>
-            </div>
-
-            <Pagination
-              newsPerPage = { newsPerPage }
-              totalNews = { data.length }
-              currentPage = { currentPage }
-              paginate = { this.paginate }>
-            </Pagination>
-              </div>
-            )
-        }
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div>
+      <News />
+    </div>
+  );
 }
 
 export default App;
